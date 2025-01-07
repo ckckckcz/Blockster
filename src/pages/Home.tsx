c22e76e9c937466aad6c2d160edc4007f575a803
ckckckcz <rriovld@gmail.com>
2025-01-07 14:01:25 +0700

    Add Google Fonts and update Home component layout and styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,14 @@
 export default function Home() {
     return(
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-white dark:bg-black">
             <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">We invest in the world’s potential</h1>
+                <a href="#" className="inline-flex justify-between items-center py-1 px-1 pe-4 mb-7 text-sm text-blue-700 bg-blue-100 rounded-full dark:bg-gray-900 dark:text-white hover:bg-blue-200 dark:hover:bg-gray-800">
+                    <span className="text-xs bg-gray-600 rounded-full text-white px-4 py-1.5 me-3">New</span> <span className="text-sm font-medium">Get Listed on Binance</span> 
+                    <svg className="w-2.5 h-2.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                    </svg>
+                </a>
+                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Best Decentralized Exchange Experience</h1>
                 <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
                 <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
                     <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
