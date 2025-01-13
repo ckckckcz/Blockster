818d93e3ac2554f97f9e1846d8bf300496dcf2bc
ckckckcz <rriovld@gmail.com>
2025-01-13 12:22:30 +0700

    Update Advantages component header to remove bottom margin for improved layout

--- src/components/UI/Home/Advantages.tsx
+++ src/components/UI/Home/Advantages.tsx
@@ -6,7 +6,7 @@ export default function Advantages() {
         <>
             <section className="bg-black">
                 <div className="py-8 px-4 mx-auto max-w-screen-2xl text-left lg:py-16">
-                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Advantages</h1>
+                    <h1 className="text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Advantages</h1>
                 </div>
                 <div className="bg-gradient-to-b from-gray-800 to-black border-t-[2px] border-gray-700">
                     <div className="mx-auto max-w-screen-2xl text-left">
